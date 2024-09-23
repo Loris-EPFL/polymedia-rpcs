5769424fac907064c6c38a6ea1c9d7f164f4cfe1
juzybits <96890594+juzybits@users.noreply.github.com>
2024-09-23 18:40:17 +0000

    modernize

--- src/web/src/App.tsx
+++ src/web/src/App.tsx
@@ -40,7 +40,7 @@ const App: React.FC = () =>
     const Header: React.FC = () =>
     {
         return <header>
-            <LinkExternal href="https://polymedia.app" follow={true}>
+            <LinkExternal html={{ href: "https://polymedia.app" }}>
                 <h1>
                     <img alt="polymedia" src="https://assets.polymedia.app/img/all/logo-nomargin-transparent-512x512.webp" className="logo" />
                 </h1>
@@ -86,10 +86,10 @@ const App: React.FC = () =>
     (
         <footer>
             <div id="icons">
-                <LinkExternal href="https://polymedia.app" follow={true}>
+                <LinkExternal html={{ href: "https://polymedia.app" }}>
                     <img alt="polymedia" src="https://assets.polymedia.app/img/all/logo-nomargin-transparent-512x512.webp" className="icon" />
                 </LinkExternal>
-                <LinkExternal href="https://github.com/juzybits/polymedia-rpcs" follow={true}>
+                <LinkExternal html={{ href: "https://github.com/juzybits/polymedia-rpcs" }}>
                     <GitHubLogo />
                 </LinkExternal>
             </div>
