55609228926c904ace402f246504a5944a5b7751
juzybits <96890594+juzybits@users.noreply.github.com>
2025-02-24 13:53:02 +0100

    modernize vite.config.ts

--- src/web/vite.config.ts
+++ src/web/vite.config.ts
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         react(),
@@ -14,5 +13,8 @@ export default defineConfig({
     },
     server: {
         port: 1234,
-    }
+    },
+    resolve: {
+        dedupe: ['react', 'react-dom']
+    },
 });
