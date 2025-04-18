0b2ea9ffe4aa1616be6e90bb3c6eee1c8cb8d549
ltran <loris.tran@epfl.ch>
2025-04-18 20:54:58 +0200

    feat: changed vite.config.ts to remove badge

--- src/web/vite.config.ts
+++ src/web/vite.config.ts
@@ -1,14 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
     plugins: [
         react(),
-        nodePolyfills({
-            // Whether to polyfill `node:` protocol imports
-            protocolImports: true,
-          }),
     ],
     build: {
         chunkSizeWarningLimit: 1000,
@@ -17,24 +12,10 @@ export default defineConfig({
         port: 1234,
     },
     server: {
+        host: '0.0.0.0', // Bind to all interfaces
         port: 1234,
-        cors: true, // This will disable CORS restrictions for the dev server
-        proxy: {
-          '/lavanet.lava.pairing.BadgeGenerator': {
-            target: 'https://badges.lavanet.xyz',
-            changeOrigin: true,
-            secure: true,
-            rewrite: (path) => path
-          }
-        }
       },
     resolve: {
-        dedupe: ['react', 'react-dom'],
-        alias: {
-            // This is needed for some packages that expect these Node.js modules
-            path: 'path-browserify',
-            stream: 'stream-browserify',
-            crypto: 'crypto-browserify',
-          },
-    }
+        dedupe: ['react', 'react-dom']
+    },
 });
